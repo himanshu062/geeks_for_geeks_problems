df4f92015084c23db0d1b878485b4636351cf77e
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-06-25 02:25:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  void rotateRowLeft(vector<int>& row, int k) {
+    int n = row.size();
+    k = k % n; 
+    if (k == 0) return;
+    vector<int> temp(row.begin() + k, row.end());
+    temp.insert(temp.end(), row.begin(), row.begin() + k);
+    row = temp;
+}
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        // code here
+        for (auto& row : mat) {
+        rotateRowLeft(row, k);
+    }
+    return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(k, mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+
+// } Driver Code Ends
