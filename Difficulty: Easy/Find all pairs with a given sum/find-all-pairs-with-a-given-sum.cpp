ef1548f2d9ad7f5f6e5172297863e263e3d9f46f
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-09-10 23:42:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find all pairs with a given sum/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find all pairs with given sum.
+    vector<pair<int, int>> allPairs(int x, vector<int> &arr1, vector<int> &arr2) {
+        // Code here
+        vector<pair<int, int>> result;
+        unordered_set<int> s(arr2.begin(), arr2.end());
+        
+        for (int u : arr1) {
+            int v = x - u;
+            if (s.find(v) != s.end()) {
+                result.push_back({u, v});
+            }
+        }
+        
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr1;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr1.push_back(number);
+        }
+        vector<int> arr2;
+        string input2;
+        getline(cin, input2); // Read the entire line for the array elements
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            arr2.push_back(number2);
+        }
+
+        Solution ob;
+        vector<pair<int, int>> vp = ob.allPairs(x, arr1, arr2);
+        int sz = vp.size();
+        if (sz == 0)
+            cout << -1 << endl;
+        else {
+            for (int i = 0; i < sz; i++) {
+                if (i == 0)
+                    cout << vp[i].first << " " << vp[i].second;
+                else
+                    cout << ", " << vp[i].first << " " << vp[i].second;
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
+// } Driver Code Ends
