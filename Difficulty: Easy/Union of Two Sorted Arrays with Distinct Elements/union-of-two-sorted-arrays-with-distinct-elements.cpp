ebd67f4e634d6b3ea8d9b6669c875a9a7ade3515
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-11-10 10:28:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        vector<int> result;
+        int i = 0, j = 0;
+        
+        while (i < a.size() && j < b.size()) {
+            if (a[i] < b[j]) {
+                result.push_back(a[i++]);
+            } else if (a[i] > b[j]) {
+                result.push_back(b[j++]);
+            } else {
+                result.push_back(a[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        while (i < a.size()) {
+            result.push_back(a[i++]);
+        }
+        
+        while (j < b.size()) {
+            result.push_back(b[j++]);
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
