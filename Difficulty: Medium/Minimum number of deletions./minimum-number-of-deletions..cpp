c9b94cb3437c927ad3069e03e63031ec59113518
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-11-20 15:02:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions./minimum-number-of-deletions..cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int minDeletions(string str, int n);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cout << minDeletions(s, n) << endl;
+
+    
+cout << "~" << "\n";
+}
+return 0;
+}
+// } Driver Code Ends
+
+
+int minDeletions(string str, int n) { 
+    //complete the function here 
+    string rev = str;
+        reverse(rev.begin(), rev.end());
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str[i - 1] == rev[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return n - dp[n][n];
+} 
