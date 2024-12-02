4d4f284193a22c5732282a4fa40a0b1c63b266ef
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-12-02 12:15:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find Prime numbers in a range/find-prime-numbers-in-a-range.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> primeRange(int M, int N) {
+        // code here
+        vector<int> primes;
+        
+        for (int i = M; i <= N; i++) {
+            if (i < 2) continue;
+            
+            bool isPrime = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime) primes.push_back(i);
+        }
+        
+        return primes;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, K;
+        cin >> M >> N;
+        Solution ob;
+        vector<int> ans = ob.primeRange(M, N);
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
