764dbdd329ea22d335272dbfe08a5427dc468889
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-12-04 20:29:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
@@ -15,6 +15,7 @@ struct Job
 
 // } Driver Code Ends
 /*
+
 struct Job 
 { 
     int id;	 // Job Id 
@@ -26,31 +27,34 @@ struct Job
 class Solution 
 {
     public:
-   bool static comp(Job a, Job b){
-        return a.profit> b.profit;
-    }
+    //Function to find the maximum profit and the number of jobs done.
     vector<int> JobScheduling(Job arr[], int n) 
     { 
-        int count=0,profit=0;
-        sort(arr,arr+n,comp);
-        int maxi=arr[0].dead;
-        for(int i=0;i<n;i++) maxi=max(maxi,arr[i].dead);
-        int slot[maxi+1];
-        for(int i=0;i<=maxi;i++) slot[i]=-1;
-        
-         int countJobs = 0, jobProfit = 0;
+        // your code here
+        sort(arr, arr + n, [](Job &a, Job &b) {
+            return a.profit > b.profit;
+        });
+
+        int maxDeadline = 0;
+        for (int i = 0; i < n; i++) {
+            maxDeadline = max(maxDeadline, arr[i].dead);
+        }
+
+        vector<int> slots(maxDeadline + 1, -1);
+        int countJobs = 0, maxProfit = 0;
 
         for (int i = 0; i < n; i++) {
-         for (int j = arr[i].dead; j > 0; j--) {
-            if (slot[j] == -1) {
-               slot[j] = i;
-               countJobs++;
-               jobProfit += arr[i].profit;
-               break;
+            for (int j = arr[i].dead; j > 0; j--) {
+                if (slots[j] == -1) {
+                    slots[j] = arr[i].id;
+                    countJobs++;
+                    maxProfit += arr[i].profit;
+                    break;
+                }
             }
-         }
-      }
-      return {countJobs,jobProfit};
+        }
+
+        return {countJobs, maxProfit};
     } 
 };
 
@@ -81,7 +85,9 @@ int main()
         //function call
         vector<int> ans = ob.JobScheduling(arr, n);
         cout<<ans[0]<<" "<<ans[1]<<endl;
-    }
+    
+cout << "~" << "\n";
+}
 	return 0; 
 }
 
