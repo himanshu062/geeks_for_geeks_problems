23a9758378049c33ff855c535f89e60cd4b30a38
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-12-12 20:33:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Beauty of All Substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int beautySum(string s) {
+        // Your code goes here
+        int n = s.size(), totalBeauty = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+                int maxFreq = 0, minFreq = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxFreq = max(maxFreq, freq[k]);
+                        minFreq = min(minFreq, freq[k]);
+                    }
+                }
+                totalBeauty += (maxFreq - minFreq);
+            }
+        }
+        return totalBeauty;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.beautySum(s) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
