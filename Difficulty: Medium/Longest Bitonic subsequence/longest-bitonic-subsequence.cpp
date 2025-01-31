f64aa8240290cb75ed5c6ddf001f6af7ad6521f8
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-01-31 21:10:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int LongestBitonicSequence(int n, vector<int> &nums) {
+        // code here
+        vector<int> inc(n, 1), dec(n, 1);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    inc[i] = max(inc[i], inc[j] + 1);
+                }
+            }
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] > nums[j]) {
+                    dec[i] = max(dec[i], dec[j] + 1);
+                }
+            }
+        }
+        
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            if (inc[i] > 1 && dec[i] > 1) {
+                maxLen = max(maxLen, inc[i] + dec[i] - 1);
+            }
+        }
+        
+        return maxLen;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        vector<int> nums(n);
+        Array::input(nums, n);
+
+        Solution obj;
+        int res = obj.LongestBitonicSequence(n, nums);
+
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
