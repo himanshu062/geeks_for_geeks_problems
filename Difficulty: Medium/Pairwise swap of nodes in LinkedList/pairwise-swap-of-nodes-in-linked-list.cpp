7618d7c02de6359edba6e7af37f01fbb1de916e7
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-02-09 12:18:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pairwise swap of nodes in LinkedList/pairwise-swap-of-nodes-in-linked-list.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to swap elements pairwise.
+struct Node* pairwise_swap(struct Node* head)
+{
+    // your code here
+    if (!head || !head->next) return head;
+    
+    Node* prev = head;
+    Node* curr = head->next;
+    head = curr;
+
+    while (true) {
+        Node* next = curr->next;
+        curr->next = prev;
+
+        if (!next || !next->next) {
+            prev->next = next;
+            break;
+        }
+
+        prev->next = next->next;
+        prev = next;
+        curr = prev->next;
+    }
+
+    return head;
+}
+
+//{ Driver Code Starts.
+void printList(struct Node* node) 
+{ 
+    while (node!=NULL) 
+    { 
+        printf("%d ", (node)->data); 
+        node = (node)->next; 
+    } 
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n, l,firstdata;
+        cin>>n;
+        // taking first node of linked list
+        cin>>firstdata;
+        struct Node* head = new Node(firstdata);
+        struct Node* tail = head;
+        // taking remaining nodes of linked list
+        for(int i = 1; i < n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        head =pairwise_swap(head);
+        printList(head);
+        cout << endl;
+    
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
