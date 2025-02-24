be85671843cbc370f67a0c4548da46b2bcbd4ca6
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-02-24 07:22:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,61 +1,49 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-
-
-class Solution
-{
-    public:
-    //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
-    {
-       // Your code here
-       vector<int> span(n);
-    stack<int> st;
-    
-    for (int i = 0; i < n; i++) {
-        while (!st.empty() && price[st.top()] <= price[i]) {
-            st.pop();
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // write code here
+        int n = arr.size();
+        vector<int>ans(n,1);
+        stack<int>st;
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && arr[st.top()] <= arr[i]) st.pop();
+            if(!st.empty()) ans[i] = i-st.top();
+            else ans[i] = i+1;
+            st.push(i);
         }
-        
-        span[i] = (st.empty()) ? (i + 1) : (i - st.top());
-        st.push(i);
-    }
-    
-    return span;
+        return ans;
     }
 };
 
-
-
 //{ Driver Code Starts.
 
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int i,a[n];
-		for(i=0;i<n;i++)
-		{
-			cin>>a[i];
-		}
-		Solution obj;
-		vector <int> s = obj.calculateSpan(a, n);
-		
-		for(i=0;i<n;i++)
-		{
-			cout<<s[i]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.calculateSpan(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
-
 // } Driver Code Ends
