1c23539a94c498a91caf27feeea2022a6933a23e
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-03-07 08:09:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    void inputData() {
+        // code here
+        int a;
+        string b;
+        cin >> a;
+        cin.ignore();
+        getline(cin, b);
+        cout << a << " " << b << endl;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        Solution obj;
+
+        obj.inputData();
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
