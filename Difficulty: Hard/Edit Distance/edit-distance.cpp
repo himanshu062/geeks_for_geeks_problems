dc3b09c50e3a4291e1c2f36562dd165135ce9d48
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-03-10 07:52:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -1,15 +1,18 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int editDistance(string word1, string word2) {
-        // Code here
-        const int m = word1.length();
-        const int n = word2.length();
+    // Function to compute the edit distance between two strings
+    int editDistance(string& s1, string& s2) {
+        // code here
+        const int m = s1.length();
+        const int n = s2.length();
         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         for (int i = 1; i <= m; ++i)
             dp[i][0] = i;
@@ -19,7 +22,7 @@ class Solution {
         {
             for(int j=1;j<=n;++j)
             {
-                if(word1[i-1] == word2[j-1])
+                if(s1[i-1] == s2[j-1])
                     dp[i][j] = dp[i-1][j-1];
                 else
                     dp[i][j] = min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]})+1;
@@ -31,15 +34,21 @@ class Solution {
 
 
 //{ Driver Code Starts.
+
 int main() {
+
     int T;
     cin >> T;
+    cin.ignore();
     while (T--) {
-        string s, t;
-        cin >> s >> t;
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
         Solution ob;
-        int ans = ob.editDistance(s, t);
+        int ans = ob.editDistance(s1, s2);
         cout << ans << "\n";
+        cout << "~" << endl;
     }
     return 0;
 }
