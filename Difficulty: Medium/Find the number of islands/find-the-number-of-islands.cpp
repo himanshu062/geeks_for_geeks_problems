7c071d1a7a187a7a1beea949504a14b4fb374948
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-04-05 21:52:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -2,39 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
-  public:
-  int n,m;
-    vector<vector<int>> dir{{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,-1},{1,-1},{-1,1}};
-    void solve(vector<vector<char>>& grid,int i,int j){
-        //base case
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0') return;
-        grid[i][j]='0';
-        for(auto &it:dir){
-            int newi=i+it[0];
-            int newj=j+it[1];
-            solve(grid,newi,newj);
-        }
-        
+  private:
+    bool isValid(int row, int col, vector<vector<char>>& grid){
+        return (row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size());
     }
-    // Function to find the number of islands.
-    int numIslands(vector<vector<char>>& grid) {
+  
+    void dfs(int row, int col, vector<vector<bool>> &vis, vector<vector<char>>& grid){
+        vis[row][col] = true;
+        
+        for(int dr=-1; dr<=1; ++dr){
+            for(int dc=-1; dc<=1; ++dc){
+                if(dr == 0 && dc == 0)  continue;
+                int nr = row + dr, nc = col + dc;
+                if(isValid(nr, nc, grid) && grid[nr][nc] == 'L' && !vis[nr][nc])
+                    dfs(nr, nc, vis, grid);
+            }
+        }
+    }
+  
+  public:
+    int countIslands(vector<vector<char>>& grid) {
         // Code here
-        n=grid.size();
-        m=grid[0].size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    solve(grid,i,j);
-                    count++;
+        int n = grid.size(), m = grid[0].size(), res = 0;
+        vector<vector<bool>> vis(n, vector<bool> (m, false));
+        
+        for(int row=0; row<n; ++row){
+            for(int col=0; col<m; ++col){
+                if(grid[row][col] == 'L' && !vis[row][col]){
+                    ++res;
+                    dfs(row, col, vis, grid);
                 }
             }
         }
-        return count;
+        return res;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
@@ -50,8 +57,11 @@ int main() {
             }
         }
         Solution obj;
-        int ans = obj.numIslands(grid);
+        int ans = obj.countIslands(grid);
         cout << ans << '\n';
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
