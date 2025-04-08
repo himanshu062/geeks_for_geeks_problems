ff7fed8823cd409b4316ccafcdfac6763fde33dc
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-04-08 20:16:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bridge edge in a graph/bridge-edge-in-a-graph.cpp
+++ Difficulty: Medium/Bridge edge in a graph/bridge-edge-in-a-graph.cpp
@@ -1,71 +1,94 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-	public:
-    //Function to find if the given edge is a bridge in graph.
-    void dfs(int node, vector<int> adj[], vector<bool>& visited) {
-        visited[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, adj, visited);
+
+class Solution {
+  public:
+    void dfs(int i,vector<int> adj[],vector<int> &vis){
+        vis[i]=1;
+        for(auto j:adj[i]){
+            if(!vis[j]){
+                dfs(j,adj,vis);
             }
         }
     }
-    int isBridge(int V, vector<int> adj[], int c, int d) 
-    {
+    bool isBridge(int n, vector<vector<int>> &edges, int c, int d) {
         // Code here
-        vector<int> tempAdj[V];
+        int ans=0;
+        vector<int> adj[n];
+        for(auto it: edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                ans++;
+                dfs(i,adj,vis);
+            }
+        }
+        //2
         
-        for (int i = 0; i < V; i++) {
-            for (int neighbor : adj[i]) {
-                if ((i == c && neighbor == d) || (i == d && neighbor == c)) {
-                    continue;
-                }
-                tempAdj[i].push_back(neighbor);
+        int ans2=0;
+        vector<int> adj2[n];
+        for(auto it: edges){
+            if(( it[0]==c && it[1]==d ) || ( it[1]==c && it[0]==d )){
+                continue;
+            }
+            adj2[it[0]].push_back(it[1]);
+            adj2[it[1]].push_back(it[0]);
+        }
+        vector<int> vis2(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis2[i]){
+                ans2++;
+                dfs(i,adj2,vis2);
             }
         }
         
-        vector<bool> visited(V, false);
-        dfs(c, tempAdj, visited);
-        
-        return !visited[d];
+        return ans!=ans2;
     }
 };
 
+
 //{ Driver Code Starts.
 
-
-int main()
-{
+int main() {
     int t;
     cin >> t;
     while (t--) {
         int V, E;
         cin >> V >> E;
         vector<int> adj[V];
-        int i=0;
-        while (i++<E) {
+        vector<vector<int>> edges(E);
+        int i = 0;
+        while (i++ < E) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back (v);
-            adj[v].push_back (u);
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            edges[i - 1].push_back(u);
+            edges[i - 1].push_back(v);
         }
-        
-        int c,d;
-        cin>>c>>d;
-        
+
+        int c, d;
+        cin >> c >> d;
+
         Solution obj;
-    	cout << obj.isBridge(V, adj, c, d) << "\n";
-    
-cout << "~" << "\n";
-}
+        bool l = obj.isBridge(V, edges, c, d);
+        if (l)
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
 
     return 0;
 }
-
 
 // } Driver Code Ends
