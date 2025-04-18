fc985ad2e483820ed29716d754498fec94b6e0ea
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-04-18 20:30:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Trie/implement-trie.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; ++i)
+            children[i] = nullptr;
+    }
+};
+class Trie {
+  public:
+    TrieNode* root;
+    
+    Trie() {
+         root = new TrieNode();
+    }
+
+    void insert(string &word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->children[idx]) {
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string &word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->children[idx]) return false;
+            node = node->children[idx];
+        }
+        return node->isEnd;
+    }
+
+    bool isPrefix(string &word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->children[idx]) return false;
+            node = node->children[idx];
+        }
+        return true;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int q;
+        cin >> q;
+        cin.ignore();
+        vector<bool> ans;
+        Trie ob;
+        for (int i = 0; i < q; i++) {
+            int x;
+            cin >> x;
+            string s;
+            cin >> s;
+            cin.ignore();
+            if (x == 1) {
+                ob.insert(s);
+            } else if (x == 2) {
+                ans.push_back(ob.search(s));
+            } else if (x == 3) {
+                ans.push_back(ob.isPrefix(s));
+            }
+        }
+        cout << boolalpha;
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
