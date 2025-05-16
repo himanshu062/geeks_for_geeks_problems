52f36031baadbf98613c4da88866c510708ac290
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2025-05-16 08:55:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest range in K lists/smallest-range-in-k-lists.cpp
+++ Difficulty: Hard/Smallest range in K lists/smallest-range-in-k-lists.cpp
@@ -1,67 +1,65 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define N 1000
 
 
 // } Driver Code Ends
-// you are required to complete this function 
-// function should print the required range
-class Solution{
-    public:
-    pair<int,int> findSmallestRange(int KSortedArray[][N], int n, int k)
-    {
-          //code here
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> minHeap;
-        int maxElement = INT_MIN;
-        for (int i = 0; i < k; i++) {
-            minHeap.push({KSortedArray[i][0], {i, 0}});
-            maxElement = max(maxElement, KSortedArray[i][0]);
+
+#define p pair<int, pair<int, int>>
+class Solution {
+  public:
+    vector<int> findSmallestRange(vector<vector<int>>& arr) {
+        // code here
+        int k = arr.size(), n = arr[0].size();
+        priority_queue<p, vector<p>, greater<p>> pq;
+        int mn = INT_MAX, mx = INT_MIN, range = INT_MAX;
+        int l=0, h=0;
+        for(int i=0; i<k; i++){
+            pq.push({arr[i][0], {i, 0}});
+            mn = min(arr[i][0], mn);
+            mx = max(arr[i][0], mx);
         }
-        int rangeStart = 0, rangeEnd = INT_MAX;
-        while (true) {
-            auto current = minHeap.top();
-            minHeap.pop();
-            int minElement = current.first;
-            int listIndex = current.second.first;
-            int elementIndex = current.second.second;
-            if (maxElement - minElement < rangeEnd - rangeStart) {
-                rangeStart = minElement;
-                rangeEnd = maxElement;
+        while(!pq.empty()){
+            p temp = pq.top();
+            pq.pop();
+            int mini = temp.first;
+            if(range > mx-mini){
+                mn = mini;
+                l = mn;
+                h = mx;
+                range = mx - mn;
             }
-            if (elementIndex + 1 < n) {
-                int nextElement = KSortedArray[listIndex][elementIndex + 1];
-                minHeap.push({nextElement, {listIndex, elementIndex + 1}});
-                maxElement = max(maxElement, nextElement);
-            } 
-            else break;
-            
+            int i = temp.second.first, j = temp.second.second;
+            if(j == n-1) 
+                break;
+            pq.push({arr[i][j+1], {i, j+1}});
+            if(mx < arr[i][j+1])
+                mx = arr[i][j+1];
         }
-        
-        return {rangeStart, rangeEnd};
+        return {l, h};
     }
 };
 
-//{ Driver Code Starts.
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, k;
-        cin>>n>>k;
-        int arr[N][N];
-        pair<int,int> rangee;
-        for(int i=0; i<k; i++)
-            for(int j=0; j<n; j++)
-                cin>>arr[i][j];
-        Solution obj;
-	    rangee = obj.findSmallestRange(arr, n, k);
-	    cout<<rangee.first<<" "<<rangee.second<<"\n";
-    }   
-	return 0;
-}
 
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<vector<int>> arr(k, vector<int>(n));
+
+        for (int i = 0; i < k; i++)
+            for (int j = 0; j < n; j++)
+                cin >> arr[i][j];
+
+        Solution obj;
+        vector<int> range = obj.findSmallestRange(arr);
+        cout << range[0] << " " << range[1] << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
 
 // } Driver Code Ends
